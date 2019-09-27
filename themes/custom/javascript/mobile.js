0245820d1271ca48f20bf7e01e1f714e53bc7528
Troy <Troy@halocreative.co.nz>
2019-09-27 12:10:59 +1000

    Update mobile.js

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,6 +1,7 @@
 ;(function($) { 
 	$('.dropdown').click(function(event){
-		if ($(this).find('.dropdown-content') && event.target.className  !== 'nav-link') {
+		console.log(event.target.className );
+		if ($(this).find('.dropdown-content') && event.target.className  !== 'nav-link child') {
 			event.preventDefault();
 			$(this).find(".dropdown-content").slideToggle( "slow");
 			console.log(event.target.className );
