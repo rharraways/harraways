528cc10244c5c888eea274a8f156fbb90a60e436
Troy <Troy@halocreative.co.nz>
2019-09-27 12:02:26 +1000

    Update mobile.js

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,9 +1,9 @@
 ;(function($) { 
 	$('.dropdown').click(function(event){
-		if ($(this).find('.dropdown-content') && event.target.nodeName !== 'A') {
+		if ($(this).find('.dropdown-content') && event.target.className  !== 'nav-link') {
 			event.preventDefault();
 			$(this).find(".dropdown-content").slideToggle( "slow");
-			console.log(event.target.nodeName);
+			console.log(event.target.className );
 		}
 	});
 })(jQuery);
