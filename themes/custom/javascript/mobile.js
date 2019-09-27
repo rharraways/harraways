06db84d1bd0da7c4fc7a471e0ca5548da64fd384
Troy <Troy@halocreative.co.nz>
2019-09-27 14:32:10 +1000

    instagram
    instagram mobile

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,4 +1,10 @@
 ;(function($) { 
+	$(".lightwidget__tile").attr('flex-basis', '100%');
+	$(".lightwidget__tile").attr('max-width', '100%');
+	$(".lightwidget__tile").attr('width', '100%');
+
+	$(".lightwidget__image").attr('width', '100%');
+
 	$('.dropdown').click(function(event){
 		console.log(event.target.className );
 		if ($(this).find('.dropdown-content') && event.target.className  !== 'nav-link child') {
