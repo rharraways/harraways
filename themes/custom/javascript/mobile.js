41c8fc38710c2a3deb866b435144d0ed93ea2c10
Troy <Troy@halocreative.co.nz>
2019-09-27 14:34:21 +1000

    Update mobile.js
    on page load

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,16 +1,18 @@
 ;(function($) { 
-	$(".lightwidget__tile").attr('flex-basis', '100%');
-	$(".lightwidget__tile").attr('max-width', '100%');
-	$(".lightwidget__tile").attr('width', '100%');
+	$(document).ready(function() {
+		$(".lightwidget__tile").attr('flex-basis', '100%');
+		$(".lightwidget__tile").attr('max-width', '100%');
+		$(".lightwidget__tile").attr('width', '100%');
 
-	$(".lightwidget__image").attr('width', '100%');
+		$(".lightwidget__image").attr('width', '100%');
 
-	$('.dropdown').click(function(event){
-		console.log(event.target.className );
-		if ($(this).find('.dropdown-content') && event.target.className  !== 'nav-link child') {
-			event.preventDefault();
-			$(this).find(".dropdown-content").slideToggle( "slow");
+		$('.dropdown').click(function(event){
 			console.log(event.target.className );
-		}
+			if ($(this).find('.dropdown-content') && event.target.className  !== 'nav-link child') {
+				event.preventDefault();
+				$(this).find(".dropdown-content").slideToggle( "slow");
+				console.log(event.target.className );
+			}
+		});
 	});
 })(jQuery);
