e39fec067015625eb60ad9cac2b7a158cb1c6b5c
Troy <Troy@halocreative.co.nz>
2019-09-27 11:22:12 +1000

    Update mobile.js

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,6 +1,6 @@
 ;(function($) { 
 	$('.dropdown').click(function(event){
-		if ($(this).find('.dropdown-content') && event.target.nodeName !== a) {
+		if ($(this).find('.dropdown-content') && event.target.nodeName !== 'a') {
 			event.preventDefault();
 			$(this).find(".dropdown-content").slideToggle( "slow");
 			Console.log(event.target.nodeName);
