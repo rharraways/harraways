5eef90afb9ed20d23b1249678c54c3d201d8791a
Troy <Troy@halocreative.co.nz>
2019-09-27 14:46:17 +1000

    JS Instagram mobile
    finding child of iframe

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,7 +1,7 @@
 ;(function($) { 
 	$(document).ready(function() {
-		$(".lightwidget__tile").attr('style', 'flex-basis: 100%; max-width: 100%; width: 100%;');
-		$(".lightwidget__image").attr('style', 'width: 100%;');
+		$(".lightwidget-widget").contents().find(".lightwidget__tile").attr('style', 'flex-basis: 100%; max-width: 100%; width: 100%;');
+		$(".lightwidget-widget").contents().find(".lightwidget__image").attr('style', 'width: 100%;');
 
 		$('.dropdown').click(function(event){
 			console.log(event.target.className );
