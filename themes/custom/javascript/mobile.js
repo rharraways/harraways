897d08504e9cf08413006a7963d123bb5a0e6d11
Troy <Troy@halocreative.co.nz>
2019-09-27 11:17:42 +1000

    Update mobile.js

--- themes/custom/javascript/mobile.js
+++ themes/custom/javascript/mobile.js
@@ -1,8 +1,9 @@
 ;(function($) { 
 	$('.dropdown').click(function(event){
-		if ($(this).find('dropdown-content')) {
+		if ($(this).find('.dropdown-content') && event.target.nodeName !== a) {
 			event.preventDefault();
 			$(this).find(".dropdown-content").slideToggle( "slow");
+			Console.log(event.target.nodeName);
 		}
 	});
 })(jQuery);
